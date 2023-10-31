853b3f8bfff2d4b07729554bc5b455c50534f42c
Aryan <aryanmaurya879@gmail.com>
2023-10-31 10:49:31 +0530

    hrs-3110 : created serviceRegistry(eureka server)

---
+++ ServiceRegistry/src/test/java/com/hmproject/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmproject.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
