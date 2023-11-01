13e7cb9eec56d4ccb6df4aaf706828fa51ed3f9b
Aryan <aryanmaurya879@gmail.com>
2023-11-01 14:02:59 +0530

    hrs-0111 : added RestTemplate communication to fetch hotel details from HotelService in RatingServiceImpl

--- Rating-Service/src/main/java/com/hmproject/rating/services/impl/RatingServiceImpl.java
+++ Rating-Service/src/main/java/com/hmproject/rating/services/impl/RatingServiceImpl.java
@@ -1,10 +1,13 @@
 package com.hmproject.rating.services.impl;
 
+import com.hmproject.rating.entities.Hotel;
 import com.hmproject.rating.entities.Rating;
 import com.hmproject.rating.repositories.RatingRepository;
 import com.hmproject.rating.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -14,6 +17,8 @@ public class RatingServiceImpl implements RatingService {
     @Autowired
     private RatingRepository ratingRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
     @Override
     public Rating create(Rating rating) {
         return ratingRepository.save(rating);
@@ -26,7 +31,21 @@ public class RatingServiceImpl implements RatingService {
 
     @Override
     public List<Rating> getRatingsByUserId(String userId) {
-        return ratingRepository.findByUserId(userId);
+
+        List<Rating> ratingsByUserId=  this.ratingRepository.findByUserId(userId);
+
+        List<Rating> ratingList = ratingsByUserId.stream().peek(rating -> {
+
+            ResponseEntity<Hotel> hotelResponseEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+
+            Hotel hotel = hotelResponseEntity.getBody();
+            rating.setHotel(hotel);
+//            return rating;
+
+        }).toList();
+
+
+        return ratingsByUserId;
     }
 
     @Override
