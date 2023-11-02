d998e9a3153c5f119b113e0a2ae7cb76d8527409
Aryan <aryanmaurya879@gmail.com>
2023-11-02 19:31:57 +0530

    hrs-0111 : Api-Gateway-> if more than one api endpoints prefixes are there

---
+++ HotelService/src/main/java/com/hmproject/hotel/controllers/StaffController.java
@@ -0,0 +1,24 @@
+package com.hmproject.hotel.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> getStaffs(){
+        List<String> list = Arrays.asList("Ram","Shyam", "Sita", "Geeta");
+
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+}
